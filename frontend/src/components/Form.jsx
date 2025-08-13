8dbd56d6fa5fa487e3064ee67bff99bd24c97466
elvisanthonyy <info.elvisanthony@gmail.com>
2025-08-13 22:09:34 +0100

    adjust

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -57,29 +57,33 @@ const Form = ({ route, method }) => {
   };
 
   return (
-    <div className="flex h-screen justify-center  w-full ">
+    <div className="flex h-screen justify-center w-full ">
       <div className=" hidden lg:block w-[50%] h-screen">
         <img className=" object-cover h-full" src={back}></img>
       </div>
-      <div className="flex  w-[100%] flex-col md:w-[50%] justify-center items-center ">
-        <h1 className=" m-3 text-[18px] lg:mt-25">{name.toUpperCase()}</h1>
+      <div className="flex  w-[100dvh] flex-col md:w-[50%] justify-center items-center ">
+        <h1 className=" m-3 -mt-10 text-[14px] md:text-[18px] lg:mt-25">
+          {name.toUpperCase()}
+        </h1>
         <form
           autoComplete="off"
-          className="flex min-h-90 h-fit w-[80%] md:w-110 lg:m-auto flex-col mt-4 border-1  border-lightTheme-border rounded-[7px] p-10 justify-evenly  items-center relative text-left bg-white"
+          className="flex px-6 m-h-80 md:min-h-90 h-fit w-[80%] md:w-110 lg:m-auto flex-col mt-4 border-1  border-lightTheme-border rounded-[7px] p-3 md:p-10 justify-evenly  items-center relative text-left bg-white"
         >
-          <img src={rift} className="h-6 m-5" alt="rift" />
+          <img src={rift} className="h-5 m-5 md:h-6 md:m-5" alt="rift" />
 
           {message == undefined ? (
             ""
           ) : (
-            <div className="text-red-700 text-xl">{message}</div>
+            <div className="text-red-700 text-sm md:text-xl">{message}</div>
           )}
           {method == "register" ? (
             <div className="flex h-fit w-[95%]  lg:w-[85%] p-2 m-2 justify-between text-sm items-center text-left bg-white-400">
-              <label className="text-gray-800">Username</label>
+              <label className="text-[10px] md:text-[16px] text-gray-800">
+                Username
+              </label>
               <input
                 type="email"
-                className="border-1 px-2 py-[3px] w-[75%] rounded-[4px]  border-gray-400 focus:outline-none"
+                className="text-[10px] md:text-[16px] border-1 px-2 py-[3px] w-[75%] rounded-[4px]  border-gray-400 focus:outline-none"
                 placeholder="Username"
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
@@ -91,9 +95,11 @@ const Form = ({ route, method }) => {
           )}
 
           <div className="flex h-fit  w-[95%]  lg:w-[85%] p-2 m-2 justify-between text-sm items-center text-left bg-white-400">
-            <label className="text-gray-800">Email</label>
+            <label className="text-[10px] md:text-[16px] text-gray-800">
+              Email
+            </label>
             <input
-              className="border-1 px-2 py-[3px] w-[75%] rounded-[4px] border-gray-400 focus:outline-none"
+              className="text-[10px] md:text-[16px] border-1 px-2 py-[3px] w-[75%] rounded-[4px] border-gray-400 focus:outline-none"
               placeholder="Email"
               type="email"
               value={email}
@@ -103,9 +109,11 @@ const Form = ({ route, method }) => {
           </div>
 
           <div className="flex h-fit  w-[95%]  lg:w-[85%] p-2 m-2 justify-between text-sm items-center text-left bg-white-400">
-            <label className="text-gray-800">Password</label>
+            <label className="text-[10px] md:text-[16px] text-gray-800">
+              Password
+            </label>
             <input
-              className="border-1 px-2 py-[3px] w-[75%] rounded-[4px] border-gray-400 focus:outline-none"
+              className="text-[10px] md:text-[16px]-md border-1 px-2 py-[3px] w-[75%] rounded-[4px] border-gray-400 focus:outline-none"
               placeholder="Password"
               type="password"
               value={password}
@@ -119,7 +127,7 @@ const Form = ({ route, method }) => {
             name={loading ? <ButtonLoading /> : name}
           />
           <Link to={`/${name === "Register" ? "login" : "register"}`}>
-            <div className="flex justify-center items-center before:bg-gray-400 before:h-[1px] before:w-[40%] before:m-3  before:content-[''] after:content-[''] after:h-[1px] after:w-[40%] after:m-3 after:bg-gray-400 w-80 ">
+            <div className="mb-3 text-[10px] md:text-[16px] flex justify-center items-center before:bg-gray-400 before:h-[1px] before:w-[25%] md:before:w-[40%] before:m-3  before:content-[''] after:content-[''] after:h-[1px] after:w-[25%] md:after:w-[40%] after:m-3 after:bg-gray-400 w-80 ">
               {name === "Register" ? "login" : "register"}
             </div>
           </Link>
